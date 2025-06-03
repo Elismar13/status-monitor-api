dcfc1b5acf881ad4fbb5b8af808dec8da153e9f5
Elismar Silva <elismarsilva.5@outlook.com>
2025-06-03 18:12:39 -0300

    init: init

---
+++ src/test/java/com/thushima/statusmonitor/StatusMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thushima.statusmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatusMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
